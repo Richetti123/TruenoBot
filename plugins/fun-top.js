a08136566cb8a9400d4e65140d1600d26ef185ab
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-08-05 02:01:39 +0200

    Add files via upload

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -1,28 +1,32 @@
 import util from 'util';
 import path from 'path';
 
+// Función para formatear el JID a @número
+const user = (jid) => '@' + jid.split('@')[0];
 
-const user = (a) => '@' + a.split('@')[0];
-function handler(m, {groupMetadata, command, conn, text, usedPrefix}) {
+function handler(m, { groupMetadata, command, conn, text, usedPrefix }) {
+    if (!text) {
+        throw `Ejemplo de uso:\n${usedPrefix}top *texto*`;
+    }
 
-  if (!text) throw `Ejemplo de uso:\n.top *texto*`;
-  const ps = groupMetadata.participants.map((v) => v.id);
-  const a = ps.getRandom();
-  const b = ps.getRandom();
-  const c = ps.getRandom();
-  const d = ps.getRandom();
-  const e = ps.getRandom();
-  const f = ps.getRandom();
-  const g = ps.getRandom();
-  const h = ps.getRandom();
-  const i = ps.getRandom();
-  const j = ps.getRandom();
-  const k = Math.floor(Math.random() * 70);
-  const x = `${pickRandom(['🤓', '😅', '😂', '😳', '😎', '🥵', '😱', '🤑', '🙄', '💩', '🍑', '🤨', '🥴', '🔥', '👇🏻', '😔', '👀', '🌚'])}`;
-  const l = Math.floor(Math.random() * x.length);
-  const vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`;
-  const top = `*${x} Top 10 ${text} ${x}*
+    // AQUI ESTÁ EL CAMBIO CLAVE: Usamos 'v.jid' en lugar de 'v.id'
+    const ps = groupMetadata.participants.map((v) => v.jid);
     
+    const a = ps.getRandom();
+    const b = ps.getRandom();
+    const c = ps.getRandom();
+    const d = ps.getRandom();
+    const e = ps.getRandom();
+    const f = ps.getRandom();
+    const g = ps.getRandom();
+    const h = ps.getRandom();
+    const i = ps.getRandom();
+    const j = ps.getRandom();
+    const k = Math.floor(Math.random() * 70);
+    const x = `${pickRandom(['🤓', '😅', '😂', '😳', '😎', '🥵', '😱', '🤑', '🙄', '💩', '🍑', '🤨', '🥴', '🔥', '👇🏻', '😔', '👀', '🌚'])}`;
+
+    const top = `*${x} Top 10 ${text} ${x}*
+   
 *1. ${user(a)}*
 *2. ${user(b)}*
 *3. ${user(c)}*
@@ -33,15 +37,15 @@ function handler(m, {groupMetadata, command, conn, text, usedPrefix}) {
 *8. ${user(h)}*
 *9. ${user(i)}*
 *10. ${user(j)}*`;
-  m.reply(top, null, {mentions: [a, b, c, d, e, f, g, h, i, j]});
-  /*conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
-    type: 'audioMessage',
-    ptt: true});*/
+
+    m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j] });
 }
+
 handler.help = handler.command = ['top'];
 handler.tags = ['fun'];
 handler.group = true;
 export default handler;
+
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)];
+    return list[Math.floor(Math.random() * list.length)];
 }
